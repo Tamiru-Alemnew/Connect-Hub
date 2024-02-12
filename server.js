5cbfe83e45ad6a0f8035fa560cd0950a93d6672d
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-02-12 14:11:23 +0300

    basic folder staructure and server setup :

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const {v4:uuiv4} =require("uuid") 
+
+const app = express()
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+
+const server = require("http").Server(app)
+app.get("/", (req, res) => {
+    res.redirect("/"+uuiv4())
+})
+
+server.listen(process.env.PORT || 3000)
